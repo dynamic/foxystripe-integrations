9065e9eebf3d0b773d0145cc893dd3f6fd5635f7
Jason Irish <jirish@dynamicdoes.com>
2014-12-05 13:17:29 -0600

    initial commit

---
+++ code/FoxyStripeIntegrationsExtension.php
@@ -0,0 +1,33 @@
+<?php
+	
+class FoxyStripeIntegrationsExtension extends Extension {
+	
+	public function addIntegrations($FoxyData) {
+		
+		$config = SiteConfig::current_site_config();
+		
+		foreach ($config->Integrations() as $integration) {
+			
+			// relay Datafeed to each Integration via curl
+			$ch = curl_init(); 
+			curl_setopt($ch, CURLOPT_URL, $integration->URL); 
+			curl_setopt($ch, CURLOPT_HEADER, FALSE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, array("FoxyData" => $FoxyData)); 
+			//if($ignore_ssl === TRUE) curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // sometimes there are problem with SSL
+			$result = curl_exec($ch);
+			curl_close($ch);
+						
+			if($result != "foxy") {
+				echo '<p>' . $integration->Name . ' failed</p>';
+			}
+			
+			SS_Log::log($integration->Name . " responded " . $result, SS_Log::WARN);
+			$result = "";
+			
+		}
+			
+	}
+	
+}
---
+++ code/FoxyStripeIntegrationsObject.php
@@ -0,0 +1,22 @@
+<?php
+	
+class FoxyStripeIntegrationsObject extends DataObject {
+	
+	private static $db = array(
+		'Name' => 'Varchar(150)',
+		'URL' => 'Varchar(255)'	
+	);
+	
+	private static $has_one = array(
+		'SiteConfig' => 'SiteConfig'
+	);
+	
+	private static $singular_name = 'Integration';
+	private static $plural_name = 'Integrations';
+	
+	private static $summary_fields = array(
+		'Name',
+		'URL'
+	);
+	
+}
